718501fe8065b3acc7b84e392c7d8d564c7cff7c
angelique <angeliquecorbisier@gmail.com>
2020-07-10 11:30:12 +0200

    updatecalculator

---
+++ calculator/src/layout/Calculator/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Screen from './Screen/Screen';
+
+const calculator = () => (
+
+<main className="calculator">
+    <Screen />
+</main>
+
+);
+
+export default calculator; 
---
+++ calculator/src/layout/Calculator/Screen/Screen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ResultScreen from './ResultScreen/ResultScreen';
+import ComputationScreen from './ComputationScreen/ComputationScreen';
+import computationScreen from './ComputationScreen/ComputationScreen';
+
+const screen = () => (
+    <section className="screen">
+        <ResultScreen />
+        <ComputationScreen />
+    </section>
+);
+
+export default screen;
